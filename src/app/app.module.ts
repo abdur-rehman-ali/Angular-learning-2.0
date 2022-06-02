20a8572ae173a82cfb76ec2bcfcc3ab8a3d8e5bf
abdur-rehman-ali <alijoyia911@gmail.com>
2022-06-02 21:46:25 +0500

    Service made for data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { TestComponent } from './test/test.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeService } from "./employee.service";
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { StudentDataComponent } from './student-data/student-data.component';
+import { StudentDataService } from './student-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     EmployeeComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    StudentDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [EmployeeService],
+  providers: [EmployeeService,StudentDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/student-data/student-data.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentDataService } from '../student-data.service';
+
+@Component({
+  selector: 'app-student-data',
+  templateUrl: './student-data.component.html',
+  styleUrls: ['./student-data.component.css']
+})
+export class StudentDataComponent implements OnInit {
+  public data : any[]=[];
+  constructor(private _student_data_service:StudentDataService) { }
+
+  ngOnInit(): void {
+    this.data = this._student_data_service.getData();
+  }
+
+}
